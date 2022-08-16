bef3c9c465d245e10f9a0b316a995e15910ab3fc
benhat907 <80671753+benhat907@users.noreply.github.com>
2022-08-16 13:58:07 +0700

    toi uu chuc nang

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -1,6 +1,5 @@
 public class RegisterModel{
     public string Email { get; set; }
-    public int IdUser { get; set; }
     public string Password { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
